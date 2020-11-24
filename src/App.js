c63980fa6291c77786a7dbe79ba105d95c6afdb8
张杰 <zhangjie@MacBook-Pro.lan>
2020-11-24 23:18:46 +0800

    feat: zz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React , { memo } from 'react';
+import React, { memo } from 'react';
 
-export default memo( function App() {
-  return(
+export default memo(function App() {
+  return (
     <div>
       <h2>ZJmusic</h2>
+      
     </div>
   );
 });
